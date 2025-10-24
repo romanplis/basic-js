86b424fc7edf6d73537fc0122c4937c237080538
Roman Plis <plisroman@gmail.com>
2025-10-25 02:59:16 +0500

    feat: add new solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,58 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(direction = true) {
+    this.direction = direction;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.library = {};
+
+    for(let i = 0; i < this.alphabet.length; i++) {
+      this.library[this.alphabet[i]] = i;
+    }
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageUpperCase = message.toUpperCase();
+    let keyUpperCase = key.toUpperCase();
+    
+    let codeWord = "";
+    let keyIndex = 0;
+    for(let i = 0; i < messageUpperCase.length; i++) {
+      if (this.alphabet.includes(messageUpperCase[i])) {
+        codeWord += this.alphabet[(this.library[messageUpperCase[i]] + this.library[keyUpperCase[keyIndex % keyUpperCase.length]]) % this.alphabet.length];
+        keyIndex++;
+      } else {
+        codeWord += messageUpperCase[i];
+      }    
+
+    }
+    return this.direction ? codeWord : codeWord.split("").reverse().join("");
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    
+    let messageUpperCase = message.toUpperCase();
+    let keyUpperCase = key.toUpperCase();
+    
+    let codeWord = "";
+    let keyIndex = 0;
+    for(let i = 0; i < messageUpperCase.length; i++) {
+      if (this.alphabet.includes(messageUpperCase[i])) {
+        codeWord += this.alphabet[(this.library[messageUpperCase[i]] - this.library[keyUpperCase[keyIndex % keyUpperCase.length]] + this.alphabet.length) % this.alphabet.length];
+        keyIndex++;
+      } else {
+        codeWord += messageUpperCase[i];
+      }    
+
+    }
+    return this.direction ? codeWord : codeWord.split("").reverse().join("");
   }
 }
 
@@ -36,3 +80,56 @@ module.exports = {
   reverseMachine: new VigenereCipheringMachine(false),
   VigenereCipheringMachine,
 };
+
+
+
+
+
+
+
+
+
+// создаем библиотеку
+// const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const library = {};
+// for(let i = 0; i < alphabet.length; i++) {
+//   library[alphabet[i]] = i;
+// }
+
+// function encrypt(message, key) {
+//   let messageUpperCase = message.toUpperCase();
+//   let keyUpperCase = key.toUpperCase();
+  
+//   let codeWord = "";
+//   let keyIndex = 0;
+//   for(let i = 0; i < messageUpperCase.length; i++) {
+//     if (alphabet.includes(messageUpperCase[i])) {
+//       codeWord += alphabet[(library[messageUpperCase[i]] + library[keyUpperCase[keyIndex % keyUpperCase.length]]) % alphabet.length];
+//       keyIndex++;
+//     } else {
+//       codeWord += messageUpperCase[i];
+//     }    
+
+//   }
+//   return codeWord;
+// }
+
+// function decrypt(message, key) {
+//   let messageUpperCase = message.toUpperCase();
+//   let keyUpperCase = key.toUpperCase();
+  
+//   let codeWord = "";
+//   let keyIndex = 0;
+//   for(let i = 0; i < messageUpperCase.length; i++) {
+//     if (alphabet.includes(messageUpperCase[i])) {
+//       codeWord += alphabet[(library[messageUpperCase[i]] - library[keyUpperCase[keyIndex % keyUpperCase.length]] + alphabet.length) % alphabet.length];
+//       keyIndex++;
+//     } else {
+//       codeWord += messageUpperCase[i];
+//     }    
+
+//   }
+//   return codeWord;
+// }
+// console.log("Encripted word: ", encrypt('attack at dawn!', 'alphonse'));
+// console.log("Decripted word: ", decrypt('AEIHQX SX DLLU!', 'alphonse'));
